d25e9f999de8695f0801bac31e9d85dc0e8e7b85
Tushar Garg <139030529+tring27@users.noreply.github.com>
2024-05-23 11:22:52 -0700

    Delete tests directory

--- tests/brevitas/test_brevitas_import.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-
-def test_import_brevitas():
-    import brevitas
-    from brevitas.function import ops_ste
--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2023, Advanced Micro Devices, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause
-
-import torch
-
-SEED = 123456
-torch.random.manual_seed(SEED)
